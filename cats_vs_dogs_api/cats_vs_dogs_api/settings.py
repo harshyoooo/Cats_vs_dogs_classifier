2fce31bd6de8958dbeb3089726ef07c4a1a49ab7
Harsh Bhosale <harshpbhosale215@gmail.com>
2025-07-22 19:20:58 +0530

    changes

--- cats_vs_dogs_api/cats_vs_dogs_api/settings.py
+++ cats_vs_dogs_api/cats_vs_dogs_api/settings.py
@@ -40,8 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'classifier',
-    'django.contrib.staticfiles'
+    'classifier'
 ]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
